ec1ec3d573d1b3c1d042a5b222e905cc1f8b4de2
Aleksander Grzyb <aleksandergrzyb@Aleksanders-MacBook-Pro.local>
2014-10-05 20:00:15 -0400

    Initial commit.

---
+++ documents_similarity/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  documents_similarity
+//
+//  Created by Aleksander Grzyb on 05/10/14.
+//  Copyright (c) 2014 Aleksander Grzyb. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ documents_similarityTests/documents_similarityTests.m
@@ -0,0 +1,40 @@
+//
+//  documents_similarityTests.m
+//  documents_similarityTests
+//
+//  Created by Aleksander Grzyb on 05/10/14.
+//  Copyright (c) 2014 Aleksander Grzyb. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface documents_similarityTests : XCTestCase
+
+@end
+
+@implementation documents_similarityTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
