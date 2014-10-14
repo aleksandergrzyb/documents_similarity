04b8a7761b8118bcf8bd271f0511a84aee5d34de
Aleksander Grzyb <aleksandergrzyb@Aleksanders-MacBook-Pro.local>
2014-10-14 07:11:10 -0700

    First version of document.

---
+++ documents_similarity/Constans.h
@@ -0,0 +1,13 @@
+//
+//  Constans.h
+//  documents_similarity
+//
+//  Created by Aleksander Grzyb on 11/10/14.
+//  Copyright (c) 2014 Aleksander Grzyb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constans : NSObject
+
+@end
---
+++ documents_similarity/Constans.m
@@ -0,0 +1,13 @@
+//
+//  Constans.m
+//  documents_similarity
+//
+//  Created by Aleksander Grzyb on 11/10/14.
+//  Copyright (c) 2014 Aleksander Grzyb. All rights reserved.
+//
+
+#import "Constans.h"
+
+@implementation Constans
+
+@end
